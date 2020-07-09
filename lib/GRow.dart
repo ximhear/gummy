d132b152bf9093d7ad738c9d0434ba64abbaeea6
gzonelee <gzonelee@gmail.com>
2020-07-09 19:09:22 +0900

    .

--- lib/GRow.dart
+++ lib/GRow.dart
@@ -12,98 +12,89 @@ class _GRowState extends State<GRow> {
   @override
   Widget build(BuildContext context) {
     return Container(
-//      color: Colors.green,
-//              padding: EdgeInsets.all(16),
-      child: Container(
-        decoration: BoxDecoration(
-          border: Border.all(
-              width: 1.0
+      child: Material(
+        color: Colors.yellow,
+        shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.all(Radius.circular(16.0)),
+            side: BorderSide(color: Colors.yellowAccent)
+      ),
+        child: InkWell(
+          splashColor: Colors.red,
+          customBorder: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(16),
           ),
-          borderRadius: BorderRadius.all(
-              Radius.circular(16.0)
-          ),
-        ),
-        child: Material(
-//          color: Colors.yellow,
-          borderRadius: BorderRadius.all(Radius.circular(16.0)),
-          child: InkWell(
-            splashColor: Colors.red,
-            customBorder: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(16),
-            ),
-            child: Container(
-                padding: EdgeInsets.fromLTRB(16, 0, 16, 16),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Container(
+          child: Container(
+              padding: EdgeInsets.fromLTRB(16, 0, 16, 16),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Container(
 //                      color: Colors.cyan,
-                      child: Row(
-                        children: [
-                          Expanded(
-                            child: Container(
-                              padding: EdgeInsets.only(top: 16),
+                    child: Row(
+                      children: [
+                        Expanded(
+                          child: Container(
+                            padding: EdgeInsets.only(top: 16),
 //                              color: Colors.lightGreen,
-                              child: Text('Title'),
-                            ),
+                            child: Text('Title'),
                           ),
-                          Container(
+                        ),
+                        Container(
 //                            color: Colors.red,
-                            child: RawMaterialButton(
-                              constraints: BoxConstraints(
-                                  minHeight: 0, minWidth: 0),
-                              materialTapTargetSize:
-                              MaterialTapTargetSize.shrinkWrap,
-                              padding: EdgeInsets.fromLTRB(16, 8, 0, 20),
-                              onPressed: () {
-                                print("menu clicked");
-                              },
-                              child: Container(
+                          child: RawMaterialButton(
+                            constraints: BoxConstraints(
+                                minHeight: 0, minWidth: 0),
+                            materialTapTargetSize:
+                            MaterialTapTargetSize.shrinkWrap,
+                            padding: EdgeInsets.fromLTRB(16, 8, 0, 20),
+                            onPressed: () {
+                              print("menu clicked");
+                            },
+                            child: Container(
 //                                color: Colors.purple,
-                                child: Row(
-                                  mainAxisSize: MainAxisSize.min,
+                              child: Row(
+                                mainAxisSize: MainAxisSize.min,
 //                                            mainAxisAlignment: MainAxisAlignment.start,
 //                                            crossAxisAlignment: CrossAxisAlignment.start,
-                                  children: [
-                                    Container(
+                                children: [
+                                  Container(
 //                                        color: Colors.red,
-                                        child: Icon(
-                                          GIcons.primitive_dot,
-                                          color: Colors.red,
-                                          size: 10,
-                                        )),
-                                    Container(
+                                      child: Icon(
+                                        GIcons.primitive_dot,
+                                        color: Colors.red,
+                                        size: 10,
+                                      )),
+                                  Container(
 //                                        color: Colors.red,
-                                        child: Icon(
-                                          GIcons.primitive_dot,
-                                          color: Colors.red,
-                                          size: 10,
-                                        )),
-                                    Container(
-                                        child: Icon(
-                                          GIcons.primitive_dot,
-                                          color: Colors.red,
-                                          size: 10,
-                                        )),
-                                  ],
-                                ),
+                                      child: Icon(
+                                        GIcons.primitive_dot,
+                                        color: Colors.red,
+                                        size: 10,
+                                      )),
+                                  Container(
+                                      child: Icon(
+                                        GIcons.primitive_dot,
+                                        color: Colors.red,
+                                        size: 10,
+                                      )),
+                                ],
                               ),
                             ),
                           ),
-                        ],
-                      ),
+                        ),
+                      ],
                     ),
-                    Container(
+                  ),
+                  Container(
 //                        color: Colors.blueAccent,
-                        padding: EdgeInsets.fromLTRB(16, 0, 0, 0),
-                        child: Text(
-                            'sdjfls sdj flsdj lsdj lsjdfl sjdlfj sdljf lsdjf lsdjf ljfsl Entry Hello, Hello l Entry Hello, Hello lEntry Hello, Hello lEntry Hello, Hello lEntry Hello, Hello lEntry Hello, Hello l x')),
-                  ],
-                )),
-            onTap: () {
-              debugPrint("row clicked");
-            },
-          ),
+                      padding: EdgeInsets.fromLTRB(16, 0, 0, 0),
+                      child: Text(
+                          'sdjfls sdj flsdj lsdj lsjdfl sjdlfj sdljf lsdjf lsdjf ljfsl Entry Hello, Hello l Entry Hello, Hello lEntry Hello, Hello lEntry Hello, Hello lEntry Hello, Hello lEntry Hello, Hello l x')),
+                ],
+              )),
+          onTap: () {
+            debugPrint("row clicked");
+          },
         ),
       ),
     );
